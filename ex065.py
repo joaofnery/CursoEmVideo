e26991ab8401a20d6c3648311070771dabf2db6b
João Fabrício Nery <121911602+joaofnery@users.noreply.github.com>
2023-01-26 23:32:44 -0300

    Add files via upload

--- ex065.py
+++ ex065.py
@@ -1,14 +1,22 @@
-tnum = 0
-cont = 0
 
-c = "s"
+num = int(input('Digite um número: '))
+tnum = num
+cont = 1
+maior = num
+menor = num
+c = input('Deseja colocar mais um número? [s/n] ')
+
 while c == "s":
+
     num = int(input('Digite um número: '))
     tnum += num
     cont += 1
-    
+    if num > maior:
+        maior = num
+    elif num < menor:
+        menor = num
     c = input('Deseja colocar mais um número? [s/n] ')
-    if c != "s":
-        media = tnum / cont
-        print('Você digitou {} números e a média deles é {}.'.format(cont, media))
+
+media = tnum / cont
+print('Você digitou {} números e a média deles é {}.'.format(cont, media))
 
