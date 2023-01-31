23e588bba93ad42e1aa738a2882933d7e2f90589
João Fabrício Nery <121911602+joaofnery@users.noreply.github.com>
2023-01-30 21:24:39 -0300

    Add files via upload

--- ex057.py
+++ ex057.py
@@ -1,4 +1,7 @@
-sexo = str(input('Qual o sexo da pessoa[M/F]? ')).strip().upper()
+#Exercício Python 57:
+# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
+# Caso esteja errado, peça a digitação novamente até ter um valor correto.
+sexo = str(input('Qual o sexo da pessoa[M/F]? ')).strip().upper()[0]
 
 while sexo != "M" and sexo != "F":
 
