c539d84ace656260d135e1e808b8157ae0a0599a
João Fabrício Nery <121911602+joaofnery@users.noreply.github.com>
2023-02-07 23:33:56 -0300

    Add files via upload

--- ex061.py
+++ ex061.py
@@ -1,7 +1,10 @@
+#Exercício Python 61:
+# Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA,
+# mostrando os 10 primeiros termos da progressão usando a estrutura while.
 a1 = int(input('Qual o primeiro termo da PA? '))
 r = int(input('Qual a razão dessa PA? '))
 a = 1
-while a != 11:
+while a <= 10:
 
     ai = a1 + r * (a - 1)
     print('a{}={}'.format(a, ai), end=" ")
--- ex062.py
+++ ex062.py
@@ -1,10 +1,13 @@
+#Exercício Python 62:
+# Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
+# O programa encerrará quando ele disser que quer mostrar 0 termos.
 a1 = int(input('Escreva o primeiro termo da PA: '))
 r = int(input('Escreva qual a razão da PA: '))
 t = 1
-c = 11
+c = 10
 termo = 1
 while termo != 0:
-    while t != c:
+    while t <= c:
         an = a1 + r * (t - 1)
         print('a{}={}'.format(t, an), end=" ")
         t += 1
